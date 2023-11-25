5672ff373e8e93d447755d02eb493cfd8eeca60d
Javier <javier.medina15@gmail.com>
2023-11-25 16:11:52 -0500

    Refactor using a class to represent the chessboard

--- knights-travails.js
+++ knights-travails.js
@@ -1,69 +1,72 @@
-function knightMoves(start, end) {
-  const moves = [
-    [1, 2],
-    [2, 1],
-    [2, -1],
-    [1, -2],
-    [-1, -2],
-    [-2, -1],
-    [-2, 1],
-    [-1, 2],
-  ];
-
-  function isValidMove(x, y) {
-    return x >= 0 && x < 8 && y >= 0 && y < 8;
+class Chessboard {
+  constructor() {
+    this.size = 8; // Size of the chessboard
+    this.board = Array.from({ length: this.size }, () =>
+      Array(this.size).fill(false),
+    );
   }
 
-  function getValidMoves([x, y]) {
+  isValidMove(x, y) {
+    return (
+      x >= 0 && x < this.size && y >= 0 && y < this.size && !this.board[x][y]
+    );
+  }
+
+  markVisited(x, y) {
+    this.board[x][y] = true;
+  }
+
+  getValidMoves([x, y]) {
+    const moves = [
+      [1, 2],
+      [2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2],
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+    ];
+
     return moves
       .map(([dx, dy]) => [x + dx, y + dy])
-      .filter(([newX, newY]) => isValidMove(newX, newY));
+      .filter(([newX, newY]) => this.isValidMove(newX, newY));
   }
 
-  function bfs() {
+  findShortestPath(start, end) {
     const queue = [[start]];
-    const visited = new Set();
-    visited.add(start.toString());
+    this.markVisited(start[0], start[1]);
 
     while (queue.length > 0) {
       const currentPath = queue.shift();
       const currentPosition = currentPath[currentPath.length - 1];
 
       if (currentPosition[0] === end[0] && currentPosition[1] === end[1]) {
-        return currentPath;
+        const moves = currentPath.map(([x, y]) => `[${x},${y}]`).join("\n    ");
+        const movesCount = currentPath.length - 1;
+        const message = `You made it in ${movesCount} moves!  Here's your path:\n    ${moves}`;
+        console.log(message);
+        return currentPath; // Return the shortest path found
       }
 
-      const possibleMoves = getValidMoves(currentPosition);
+      const possibleMoves = this.getValidMoves(currentPosition);
+
       possibleMoves.forEach((move) => {
         const path = [...currentPath, move];
-        const key = move.toString();
+        const [x, y] = move;
 
-        if (!visited.has(key)) {
-          visited.add(key);
-          queue.push(path);
-        }
+        this.markVisited(x, y);
+        queue.push(path);
       });
     }
 
     console.log("No valid path found.");
-    return [];
+    return null; // If no valid path found
   }
-
-  const shortestPath = bfs();
-  if (shortestPath.length > 0) {
-    console.log(
-      `You made it in ${shortestPath.length - 1} moves! Here's your path:`,
-    );
-    shortestPath.forEach((position) => {
-      console.log(position);
-    });
-    return shortestPath;
-  }
-  return shortestPath;
 }
 
-// Test cases
-knightMoves([0, 0], [1, 2]);
-knightMoves([0, 0], [3, 3]);
-knightMoves([3, 3], [0, 0]);
-knightMoves([0, 0], [7, 7]);
+// Usage example:
+const chessboard = new Chessboard();
+const start = [0, 0];
+const end = [3, 3];
+chessboard.findShortestPath(start, end);
